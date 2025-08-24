03cbc9ec3cb77e65cc53bde8f4a95e474f58cf1a
Zifrikc <zuran210@gmail.com>
2025-08-24 02:36:48 -0500

    styles plus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './core/layout/navbar/navbar.component';
 import { HeroComponent } from './features/hero/hero.component';
 import { ProjectsComponent } from './features/projects/projects.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
+import { SkillsComponent } from './features/skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeroComponent, ProjectsComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, ProjectsComponent, FooterComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
